1f2f0d4861652676c1cced78e5adf593f935748f
Bobby Wilson <bobbywilson0@gmail.com>
2011-06-08 08:14:53 -0600

    change to bubbles from pie

--- repoviz_app.rb
+++ repoviz_app.rb
@@ -22,17 +22,11 @@ class RepovizApp < Sinatra::Base
   get '/:username/:project/committers.json' do
     repovis = Repoviz::Repo.new(params[:username], params[:project])
     repovis.pull
-    committers = {}
-    start_date = params[:start_date] || (Date.today).to_time
-    repovis.grit_repo.commits('master', 20).each do |commit|
-      puts commit.date
-      committers[commit.author.to_s] ||= 0 
-      committers[commit.author.to_s] += commit.stats.total
-    end
+    commits = {:commits => []}
+    repovis.grit_repo.commits('master', 20).each { |commit| commits[:commits] << { :author => commit.author, :total => commit.stats.total } }
+      
 
-    denormalized_committers = committers.map { |name, total| {:name => name, :total => total}}
-    
     content_type :json
-    Yajl::Encoder.encode(denormalized_committers)
+    Yajl::Encoder.encode(commits)
   end
 end
